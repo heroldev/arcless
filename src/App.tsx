d9760053ad343edb220b25c2ea94c03fbbfa0859
Andrew Herold <heroldaksd@gmail.com>
2023-07-19 23:38:56 -0500

    do the things

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,17 @@ function App() {
         </p>
       </div>
       <div className={"x-container"}>
-        <p><a href="https://github.com/heroldev">github</a>{" / "}<a href="https://youtube.com">list of projects</a></p>
+        <p><a href="https://github.com/heroldev">github</a>{" / "}<a href="https://storage.cloud.google.com/heroldev/resume.pdf">résumé</a>{" / "}<a href="https://youtube.com">list of projects</a></p>
       </div>
       <hr />
       <div className={"x-container"}>
         <p>email: andrew [at] this domain</p>
       </div>
-      <div className={"x-container"}>
+      {/*
+<div className={"x-container"}>
         <p className="x-p-small">hello!</p>
       </div>
+  */}
       <div className={"x-container"}>
         <p className="x-p-footer">last updated: 07.11.2023</p>
       </div>
