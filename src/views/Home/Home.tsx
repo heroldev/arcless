2dc4a1414e86083dd294e340cbdd4dac0be79cb4
Andrew Herold <heroldaksd@gmail.com>
2023-07-24 21:21:52 -0500

    reorg

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,33 @@
+import "./Home.less"
+
+const Home = () => {
+  return (
+    <>
+      <div className={"x-container"}>
+        <h1 className={"x-title-name"}>Andrew Herold</h1>
+        <h2 className={"x-subtitle"}>turning dreams into reality through efficient code and clean design</h2>
+      </div>
+      <hr />
+      <div className="x-container">
+        <p>
+          bs in computer science @ <a href="https://mnsu.edu">minnesota state university, mankato</a>
+        </p>
+      </div>
+      <div className={"x-container"}>
+        <p><a href="https://github.com/heroldev">github</a>{" / "}<a href="https://youtube.com">list of projects</a></p>
+      </div>
+      <hr />
+      <div className={"x-container"}>
+        <p>email: andrew [at] this domain</p>
+      </div>
+      <div className={"x-container"}>
+        <p className="x-p-small">hello!</p>
+      </div>
+      <div className={"x-container"}>
+        <p className="x-p-footer">last updated: 07.11.2023</p>
+      </div>
+    </>
+  )
+}
+
+export default Home
